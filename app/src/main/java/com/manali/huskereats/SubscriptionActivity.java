353014c12de69afe0d9a1cb1f7e0c3dfd847cb28
Atharva Tendle <atharva.tendle@huskers.unl.edu>
2019-01-20 04:21:39 -0600

    Profile activity (#9)
    Added Profile Activity

--- app/src/main/java/com/manali/huskereats/SubscriptionActivity.java
+++ app/src/main/java/com/manali/huskereats/SubscriptionActivity.java
@@ -1,14 +1,10 @@
 package com.manali.huskereats;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
 public class SubscriptionActivity extends AppCompatActivity {
     private Button subscribe;
     @Override
@@ -20,9 +16,9 @@ public class SubscriptionActivity extends AppCompatActivity {
         subscribe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent homeIntent = new Intent(SubscriptionActivity.this, HomeActivity.class);
-//                homeIntent.putExtra("subscribed", true);
-//                startActivity(homeIntent);
+                Intent homeIntent = new Intent(SubscriptionActivity.this, HomeActivity.class);
+                homeIntent.putExtra("subscribed", true);
+                startActivity(homeIntent);
             }
         });
     }
