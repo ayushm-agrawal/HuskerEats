a2cba3ee4762d55bdfdd45420833c29f7ce9d0d0
pranavgupta97 <pranavgupta9797@gmail.com>
2019-01-20 07:22:24 -0600

    Added functionality to MenuActivity and MenuAdpater

--- app/src/main/java/com/manali/huskereats/MenuActivity.java
+++ app/src/main/java/com/manali/huskereats/MenuActivity.java
@@ -1,9 +1,13 @@
 package com.manali.huskereats;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +16,7 @@ public class MenuActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     MenuAdapter menuAdapter;
+    Button placeOrderButton;
 
     List<MenuItem> itemList;
 
@@ -21,15 +26,26 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         itemList = new ArrayList<>();
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerMenu);
+        recyclerView = findViewById(R.id.recyclerMenu);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        placeOrderButton = findViewById(R.id.btnPlaceOrder);
+
 
         itemList.add(new MenuItem(1, "Spicy Italian", R.drawable.spicy_italian));
         itemList.add(new MenuItem(1, "Sweet Onion Teriyaki", R.drawable.sweet_onion_teriyaki));
+        itemList.add(new MenuItem(1, "Oven Roasted Chicken", R.drawable.oven_roasted_chicken));
+        itemList.add(new MenuItem(1, "Meatball Marinanra", R.drawable.meatball_marinara));
 
         menuAdapter = new MenuAdapter(this, itemList);
         recyclerView.setAdapter(menuAdapter);
+        placeOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(view.getContext(), "Hello Bois!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/manali/huskereats/MenuAdapter.java
+++ app/src/main/java/com/manali/huskereats/MenuAdapter.java
@@ -1,22 +1,30 @@
 package com.manali.huskereats;
 
 import android.content.Context;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder>{
 
     private Context context;
     private List<MenuItem> itemList;
+    private List<String> orderList = new ArrayList<String>();
 
-    public MenuAdapter(MenuActivity menuActivity, List<MenuItem> itemList) {
+    public MenuAdapter(Context context, List<MenuItem> itemList) {
+        this.context = context;
         this.itemList = itemList;
     }
 
@@ -24,17 +32,24 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder
     @Override
     public MenuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(context);
-        View view = layoutInflater.inflate(R.layout.menu_item, null);
+        View view = layoutInflater.inflate(R.layout.menu_item, parent, false);
         MenuViewHolder menuViewHolder = new MenuViewHolder(view);
         return menuViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MenuViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final MenuViewHolder holder, int position) {
         MenuItem menuItem = itemList.get(position);
 
         holder.itemName.setText(menuItem.getItemName());
         holder.foodImage.setImageDrawable(context.getResources().getDrawable(menuItem.getImage()));
+        holder.itemCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                orderList.add(holder.itemName.getText().toString());
+                Toast.makeText(view.getContext(), holder.itemName.getText().toString() + " added to cart!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -46,12 +61,15 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder
 
         ImageView foodImage;
         TextView itemName;
+        CardView itemCard;
+        FloatingActionButton shoppingCart;
 
         public MenuViewHolder(View itemView) {
             super(itemView);
 
             foodImage = itemView.findViewById(R.id.foodImage);
             itemName = itemView.findViewById(R.id.itemName);
+            itemCard = itemView.findViewById(R.id.itemCard);
         }
     }
 }
