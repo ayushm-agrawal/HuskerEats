7cfc02a86de82bb0ecb5202a0a42baeaa369d1ff
Atharva Tendle <atharva.tendle@huskers.unl.edu>
2019-01-20 07:12:58 -0600

    m

--- app/src/main/java/com/manali/huskereats/RoleActivity.java
+++ app/src/main/java/com/manali/huskereats/RoleActivity.java
@@ -24,8 +24,6 @@ public class RoleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_role);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         // get checkbox view
         final CheckBox deliverCheckBox = findViewById(R.id.deliverCheck);
