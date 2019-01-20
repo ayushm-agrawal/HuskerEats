3579ac68cad28e77f87ba04c255c41736a1a58e6
Atharva Tendle <atharva.tendle@huskers.unl.edu>
2019-01-20 03:41:23 -0600

    Update SubscriptionActivity.java
    Removed unnecessary imports

--- app/src/main/java/com/manali/huskereats/SubscriptionActivity.java
+++ app/src/main/java/com/manali/huskereats/SubscriptionActivity.java
@@ -6,22 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.wallet.AutoResolveHelper;
-import com.google.android.gms.wallet.IsReadyToPayRequest;
-import com.google.android.gms.wallet.PaymentData;
-import com.google.android.gms.wallet.PaymentDataRequest;
-import com.google.android.gms.wallet.PaymentsClient;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.util.Optional;
 
 public class SubscriptionActivity extends AppCompatActivity {
     private Button subscribe;
