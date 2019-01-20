e9a9f320099c384313a9a1d316f79295c4245718
Atharva Tendle <atharva.tendle@huskers.unl.edu>
2019-01-20 02:55:30 -0600

    meh

--- app/src/main/java/com/manali/huskereats/SubscriptionActivity.java
+++ app/src/main/java/com/manali/huskereats/SubscriptionActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.Status;
@@ -17,156 +19,25 @@ import com.google.android.gms.wallet.IsReadyToPayRequest;
 import com.google.android.gms.wallet.PaymentData;
 import com.google.android.gms.wallet.PaymentDataRequest;
 import com.google.android.gms.wallet.PaymentsClient;
-import com.google.android.gms.wallet.Wallet;
-import com.google.android.gms.wallet.WalletConstants;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.Optional;
 
 public class SubscriptionActivity extends AppCompatActivity {
-    private PaymentsClient mPaymentsClient;
-    private View mGooglePayButton;
-    private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 42;
-
+    private Button subscribe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscription);
 
-        // initialize google pay api client
-        mPaymentsClient =
-                Wallet.getPaymentsClient(
-                        this,
-                        new Wallet.WalletOptions.Builder()
-                        .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
-                        .build()
-                );
-        possiblyShowGooglePayButton();
-
-    }
-    /**
-     * Determine the viewer's ability to pay with a payment method supported by your app and display a
-     * Google Pay payment button
-     */
-    private void possiblyShowGooglePayButton() {
-        final Optional<JSONObject> isReadyToPayJson = GooglePay.getIsReadyToPayRequest();
-        if (!isReadyToPayJson.isPresent()) {
-            return;
-        }
-        IsReadyToPayRequest request = IsReadyToPayRequest.fromJson(isReadyToPayJson.get().toString());
-        if (request == null) {
-            return;
-        }
-        Task<Boolean> task = mPaymentsClient.isReadyToPay(request);
-        task.addOnCompleteListener(
-                new OnCompleteListener<Boolean>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Boolean> task) {
-                        try {
-                            boolean result = task.getResult(ApiException.class);
-                            if (result) {
-                                // show Google as a payment option
-                                mGooglePayButton = findViewById(R.id.btnSubscribe);
-                                mGooglePayButton.setOnClickListener(
-                                        new View.OnClickListener() {
-                                            @Override
-                                            public void onClick(View view) {
-                                                requestPayment(view);
-                                            }
-                                        });
-                                mGooglePayButton.setVisibility(View.VISIBLE);
-                            }
-                        } catch (ApiException exception) {
-                            // handle developer errors
-                        }
-                    }
-                });
-    }
-    /**
-     * Display the Google Pay payment sheet after interaction with the Google Pay payment button
-     */
-    public void requestPayment(View v){
-        Optional<JSONObject> paymentDataRequestJson  = GooglePay.getPaymentDataRequest();
-        if(!paymentDataRequestJson.isPresent()){
-            return;
-        }
-        PaymentDataRequest request =
-                PaymentDataRequest.fromJson(paymentDataRequestJson.get().toString());
-        if (request != null){
-            AutoResolveHelper.resolveTask(
-                    mPaymentsClient.loadPaymentData(request), this, LOAD_PAYMENT_DATA_REQUEST_CODE
-            );
-        }
-    }
-    /**
-     * Handle a resolved activity from the Google Pay payment sheet
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            // value passed in AutoResolveHelper
-            case LOAD_PAYMENT_DATA_REQUEST_CODE:
-                switch (resultCode) {
-                    case Activity.RESULT_OK:
-                        PaymentData paymentData = PaymentData.getFromIntent(data);
-                        String json = paymentData.toJson();
-                        // if using gateway tokenization, pass this token without modification
-                        handlePaymentSuccess(paymentData);
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        break;
-                    case AutoResolveHelper.RESULT_ERROR:
-                        Status status = AutoResolveHelper.getStatusFromIntent(data);
-                        // Log the status for debugging.
-                        // Generally, there is no need to show an error to the user.
-                        // The Google Pay payment sheet will present any account errors.
-                        break;
-                    default:
-                        // Do nothing.
-                }
-                break;
-            default:
-                // Do nothing.
-            // Re-enables the Google Pay payment button.
-            mGooglePayButton.setClickable(true);
-            break;
-        }
-    }
-    private void handlePaymentSuccess(PaymentData paymentData) {
-        String paymentInformation = paymentData.toJson();
-
-        // Token will be null if PaymentDataRequest was not constructed using fromJson(String).
-        if (paymentInformation == null) {
-            return;
-        }
-        JSONObject paymentMethodData;
-
-        try {
-            paymentMethodData = new JSONObject(paymentInformation).getJSONObject("paymentMethodData");
-            // If the gateway is set to "example", no payment information is returned - instead, the
-            // token will only consist of "examplePaymentMethodToken".
-            if (paymentMethodData
-                    .getJSONObject("tokenizationData")
-                    .getString("type")
-                    .equals("PAYMENT_GATEWAY")
-                    && paymentMethodData
-                    .getJSONObject("tokenizationData")
-                    .getString("token")
-                    .equals("examplePaymentMethodToken")) {
-                AlertDialog alertDialog =
-                        new AlertDialog.Builder(this)
-                                .setTitle("Warning")
-                                .setMessage(
-                                        "Gateway name set to \"example\" - please modify "
-                                                + "Constants.java and replace it with your own gateway.")
-                                .setPositiveButton("OK", null)
-                                .create();
-                alertDialog.show();
+        subscribe = findViewById(R.id.btnSubscribe);
+        subscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(SubscriptionActivity.this, HomeActivity.class);
+                homeIntent.putExtra("subscribed", true);
+                startActivity(homeIntent);
             }
-
-        } catch (JSONException e) {
-            //
-        }
+        });
     }
-
 }
